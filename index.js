c054927113657bf43914fcc512e7d353cbcb6f12
Dallas <dallas.clarke622@gmail.com>
2021-04-04 14:35:22 -0700

    Tying it together

--- index.js
+++ index.js
@@ -5,26 +5,7 @@ Characteristics of a Pure Function
 3) Never produce any side effects.
 */
 
-// REDUCER FUNCTION
-function todos(state = [], action) {
-  switch (action.type) {
-    case "ADD_TODO":
-      return state.concat([action.todo]);
-    case "REMOVE_TODO":
-      return state.filter((todo) => todo.id !== action.id);
-
-    case "TOGGLE_TODO":
-      return state.map((todo) =>
-        todo.id !== action.id
-          ? todo
-          : Object.assign({}, todo, { complete: !todo.complete })
-      );
-
-    default:
-      return state;
-  }
-}
-
+// Library Code
 function createStore(reducer) {
   // The store has four parts
   // 1. The State
@@ -56,3 +37,115 @@ function createStore(reducer) {
     dispatch,
   };
 }
+
+
+// App Code
+
+// Action Types
+const ADD_TODO = "ADD_TODO";
+const REMOVE_TODO = "REMOVE_TODO";
+const TOGGLE_TODO = "TOGGLE_TODO";
+
+const ADD_GOAL = "ADD_GOAL";
+const REMOVE_GOAL = "REMOVE_GOAL";
+
+// Action Creators
+function addTodoAction(todo) {
+  return {
+    type: ADD_TODO,
+    todo,
+  }
+}
+
+function removeTodoAction(id) {
+  return {
+    type: REMOVE_TODO,
+    id,
+  }
+}
+
+function toggleTodoAction(id) {
+  return {
+    type: TOGGLE_TODO,
+    id,
+  }
+}
+
+function addGoalAction(goal) {
+  return {
+    type: ADD_GOAL,
+    goal,
+  }
+}
+
+function removeGoalAction(id) {
+  return {
+    type: REMOVE_GOAL,
+    id,
+  }
+}
+
+// REDUCER FUNCTION
+function todos(state = [], action) {
+  switch (action.type) {
+    case ADD_TODO:
+      return state.concat([action.todo]);
+    case REMOVE_TODO:
+      return state.filter((todo) => todo.id !== action.id);
+
+    case TOGGLE_TODO:
+      return state.map((todo) =>
+        todo.id !== action.id
+          ? todo
+          : Object.assign({}, todo, { complete: !todo.complete })
+      );
+
+    default:
+      return state;
+  }
+}
+
+function goals(state = [], action) {
+  switch (action.type) {
+    case ADD_GOAL:
+      return state.concat([action.goal]);
+    case REMOVE_GOAL:
+      return state.filter((goal) => goal.id !== action.id);
+
+    default:
+      return state;
+  }
+}
+
+function app(state = {}, action) {
+  return {
+    todos: todos(state.todos, action),
+    goals: goals(state.goals, action)
+  }
+}
+
+const store = createStore(app);
+
+store.dispatch(addTodoAction({
+  id: 0,
+  name: 'Walk the dog',
+  complete: false
+}))
+
+store.dispatch(addTodoAction({
+  id: 1,
+  name: "wash car",
+  complete: false
+}))
+
+store.dispatch(removeTodoAction(1))
+store.dispatch(toggleTodoAction(0))
+
+store.dispatch(addGoalAction({
+  id: 0,
+  name: "Learn Redux"
+}))
+store.dispatch(addGoalAction({
+  id: 1,
+  name: "Lose 20 pounds"
+}))
